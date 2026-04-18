8581f8a4cc8a0ba526d7b1d3c095820baca57469
Sy4688 <saranya.senthil4688@gmail.com>
2026-04-18 17:16:17 +0530

    fix cors

--- backend/faculty-service/src/main/java/com/example/faculty_service/controller/FacultyController.java
+++ backend/faculty-service/src/main/java/com/example/faculty_service/controller/FacultyController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/faculty")
 public class FacultyController {
--- backend/student-service/src/main/java/com/example/student_service/controller/StudentController.java
+++ backend/student-service/src/main/java/com/example/student_service/controller/StudentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/students")
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "*")
 public class StudentController {
 
     @Autowired
